e88a1a853922f3492e5ecad74cb8f00ab72bc9ac
patdaburu <pat@daburu.net>
2020-03-07 11:31:03 -0600

    Added conftest module.

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import NamedTuple
+import testing.postgresql
+import pytest
+
+
+class TmpDb(NamedTuple):
+    """Temporary testing database details."""
+    pgdb: testing.postgresql.Postgresql  #: testing db object
+    dburl: str  #: the database URL
+
+
+@pytest.yield_fixture(scope='module', name='tmp_db')
+def tmp_db():
+    """
+    Generates a temporal database using the testing.postgresql package
+    """
+
+    # Initialize the temporary PostgreSQL instance.
+    pgdb = testing.postgresql.Postgresql()
+
+    # Get URL and other derived data.
+    dburl = pgdb.url()
+
+    yield TmpDb(
+        pgdb=pgdb,
+        dburl=dburl
+    )
+
+    # Shut down the temporary PostgreSQL instance and clean up the temporary
+    # working directory.
+    pgdb.stop()
